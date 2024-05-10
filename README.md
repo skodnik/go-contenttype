6abe4d5d909243d573f3a2a5c79f2042a092adce
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2024-05-10 16:09:25 +0200

    Add base types

---
+++ README.md
@@ -0,0 +1,16 @@
+# Go ContentType Package
+
+This package provides a collection of common MIME-type constants. These constants can be used in various contexts, such as setting HTTP response headers, validating file types, and other related scenarios.
+
+## Features
+
+- Provides constants for common MIME types like `application/json`, `text/html`, `image/jpeg`, and more.
+- Easy to install and use in other Go projects.
+
+## Installation
+
+To install the package, use the following command:
+
+```shell
+go get github.com/skodnik/go-contenttype
+```
---
+++ contenttype/contenttype.go
@@ -0,0 +1,38 @@
+package contenttype
+
+const (
+	TextPlain      = "text/plain"
+	TextHTML       = "text/html"
+	TextCSS        = "text/css"
+	TextJavaScript = "text/javascript"
+	TextXML        = "text/xml"
+)
+
+const (
+	ApplicationJSON           = "application/json"
+	ApplicationXML            = "application/xml"
+	ApplicationPDF            = "application/pdf"
+	ApplicationZip            = "application/zip"
+	ApplicationOctetStream    = "application/octet-stream"
+	ApplicationFormURLEncoded = "application/x-www-form-urlencoded"
+)
+
+const (
+	ImageJPEG = "image/jpeg"
+	ImagePNG  = "image/png"
+	ImageGIF  = "image/gif"
+	ImageBMP  = "image/bmp"
+	ImageSVG  = "image/svg+xml"
+)
+
+const (
+	AudioMPEG = "audio/mpeg"
+	AudioWAV  = "audio/wav"
+	VideoMP4  = "video/mp4"
+	VideoAVI  = "video/x-msvideo"
+)
+
+const (
+	MultipartFormData = "multipart/form-data"
+	MessageHTTP       = "message/http"
+)
